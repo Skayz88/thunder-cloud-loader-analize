092d4e1b46185aa882903b67ab557dc0132e55a9
DRakovskiy <DRakovskiy@inno.tech>
2026-01-20 19:01:50 +0300

    no message

---
+++ src/main/java/ru/thundercloud/thunder_cloud_loader_analize/ThunderCloudLoaderAnalizeApplication.java
@@ -0,0 +1,13 @@
+package ru.thundercloud.thunder_cloud_loader_analize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderCloudLoaderAnalizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThunderCloudLoaderAnalizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/thundercloud/thunder_cloud_loader_analize/ThunderCloudLoaderAnalizeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.thundercloud.thunder_cloud_loader_analize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderCloudLoaderAnalizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
